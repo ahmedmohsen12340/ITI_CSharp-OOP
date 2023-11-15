0187f06b786621e94cf6db8ce57e3b2308ffa7a3
ahmedmohsen12340 <ahmedmohsen12340@gmail.com>
2023-11-15 09:24:03 +0200

    1-oop C# D1&

---
+++ 1-day1Lab/day1Lab/day1Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace day1Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first, second,result=0;
+            char op;
+            Console.Write("Enter first number: ");
+            first = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            second = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Operator (+ - * /)");
+            op = char.Parse(Console.ReadLine());
+            switch(op)
+            {
+                case '+':
+                    result = first + second;
+                    break;
+                case '-':
+                    result = first - second;
+                    break;
+                case '*':
+                    result = first * second;
+                    break;
+                case '/':
+                    result = first / second;
+                    break;
+                default:
+                    Console.WriteLine("enter a valid operator (+ - * /)");
+                    break;
+            }
+            Console.WriteLine($"{first} {op} {second} = {result}");
+        }
+    }
+}
---
+++ 2-day2Lab/Day2/Day2Lab/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Day2Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region problem 1
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("enter a number:");
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+            }
+            Console.WriteLine($"the sum ={sum}");
+            #endregion
+
+            #region problem 2
+            int sum2 = 0;
+            while (sum2 < 100)
+            {
+                Console.WriteLine("enter a number:");
+                int num = int.Parse(Console.ReadLine());
+                sum2 += num;
+                Console.WriteLine($"sum = {sum2}");
+            }
+            Console.WriteLine($"sum exceed 100, sum={sum2} the loop ended");
+            #endregion
+
+            #region problem 3
+            char key = ' ';
+            bool cond = true;
+            do
+            {
+                Console.WriteLine(
+                    "a: new\n" +
+                    "b: display\n" +
+                    "e: Exit"
+                    );
+                key = char.Parse(Console.ReadLine());
+                key = char.ToLower(key);
+                switch (key)
+                {
+                    //case 'A':
+                    case 'a':
+                        Console.WriteLine("new");
+                        break;
+                    //case 'B':
+                    case 'b':
+                        Console.WriteLine("Display");
+                        break;
+                    //case 'E':
+                    case 'e':
+                        cond = false;
+                        break;
+                    default:
+                        Console.WriteLine("enter a valid character (a b e)");
+                        break;
+                }
+
+            } while (cond);
+
+            #endregion        
+        }
+    }
+}
